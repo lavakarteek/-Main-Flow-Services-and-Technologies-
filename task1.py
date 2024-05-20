c97f1617623e7feb875030b2fec648421029f9c7
lavakarteek <124698056+lavakarteek@users.noreply.github.com>
2024-05-20 23:19:36 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,36 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("Original list:", my_list)
+# Adding an element to the list
+my_list.append(6)
+print("After adding an element:", my_list)
+# Modifying an element in the list
+my_list[2] = 10
+print("After modifying an element:", my_list)
+# Removing an element from the list
+my_list.remove(4)
+print("After removing an element:", my_list)
+print()
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print("Original dictionary:", my_dict)
+# Adding a key-value pair to the dictionary
+my_dict['d'] = 4
+print("After adding a key-value pair:", my_dict)
+# Modifying a value in the dictionary
+my_dict['b'] = 20
+print("After modifying a value:", my_dict)
+# Removing a key-value pair from the dictionary
+del my_dict['a']
+print("After removing a key-value pair:", my_dict)
+print()
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("Original set:", my_set)
+# Adding an element to the set
+my_set.add(6)
+print("After adding an element:", my_set)
+# Removing an element from the set
+my_set.remove(3)
+print("After removing an element:", my_set)
+
